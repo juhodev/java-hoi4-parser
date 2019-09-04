c71997256d27ca12d5fc9cd72c641711e55671ea
juhodev <juhsep4@live.kktavastia.fi>
2019-09-04 23:57:56 +0300

    Remove unused methods

--- src/main/java/dev/juho/hoi4/utils/Utils.java
+++ src/main/java/dev/juho/hoi4/utils/Utils.java
@@ -1,10 +1,5 @@
 package dev.juho.hoi4.utils;
 
-import dev.juho.hoi4.parser.textparser.HOIKey;
-import dev.juho.hoi4.parser.textparser.ast.ASTNode;
-import dev.juho.hoi4.parser.textparser.ast.nodes.ObjectNode;
-import dev.juho.hoi4.parser.textparser.ast.nodes.PropertyNode;
-
 import java.util.HashMap;
 
 public class Utils {
@@ -17,30 +12,5 @@ public class Utils {
 		}
 
 		return false;
-	}
-
-	public static HashMap<String, String> getArgs(String[] args) {
-		HashMap<String, String> map = new HashMap<>();
-
-		String key = "";
-		String value = "";
-
-		for (int i = 0; i < args.length; i++) {
-			if (args[i].startsWith("-")) {
-				if (!key.isEmpty()) {
-					map.put(key, value.trim());
-				}
-
-				key = args[i];
-			} else {
-				value += args[i] + " ";
-			}
-		}
-
-		if (!key.isEmpty()) {
-			map.put(key, value.trim());
-		}
-
-		return map;
 	}
 }
