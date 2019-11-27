aa89fab67646fcf5801aee84448c44fc1e26f83b
juhodev <jooo.dev@gmail.com>
2019-11-27 17:47:39 +0200

    Add tests for comments and whitespace at the end of files

--- src/test/java/dev/juho/hoi4/textparser/token/TextTokenizerTest.java
+++ src/test/java/dev/juho/hoi4/textparser/token/TextTokenizerTest.java
@@ -95,4 +95,37 @@ public class TextTokenizerTest {
 		Assert.assertEquals(TextParserToken.Type.STRING, tokenizer.next().getType());
 		Assert.assertEquals(TextParserToken.Type.END_OBJECT, tokenizer.next().getType());
 	}
+
+	@Test
+	public void testTokenizerSkipComments() {
+		String str = "# this comment should be ignored\n\r#secondcomment\na=2";
+
+		TextTokenizer tokenizer = new TextTokenizer(128);
+		try {
+			tokenizer.readInputStream(new ByteArrayInputStream(str.getBytes()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(TextParserToken.Type.STRING, tokenizer.next().getType());
+		Assert.assertEquals(TextParserToken.Type.OPERATION, tokenizer.next().getType());
+		Assert.assertEquals(TextParserToken.Type.STRING, tokenizer.next().getType());
+	}
+
+	@Test
+	public void testTokenizerEndsWithWhitespace() {
+		String str = "a=2\r\n ";
+
+		TextTokenizer tokenizer = new TextTokenizer(128);
+		try {
+			tokenizer.readInputStream(new ByteArrayInputStream(str.getBytes()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(TextParserToken.Type.STRING, tokenizer.next().getType());
+		Assert.assertEquals(TextParserToken.Type.OPERATION, tokenizer.next().getType());
+		Assert.assertEquals(TextParserToken.Type.STRING, tokenizer.next().getType());
+	}
+
 }
